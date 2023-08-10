15b0a23545c6eaa65bc97afa6855d16b58ebd5aa
Juliano Amaral <jaygovinda13@gmail.com>
2023-08-10 13:31:43 -0300

    pagina de autentificação ok

--- netflix/pages/auth.tsx
+++ netflix/pages/auth.tsx
@@ -1,21 +1,71 @@
-const Auth = () => {
-    return (
-        <div className="relative h-full w-full bg-[url('/images/background.jpg')] bg-no-repeat bg-center bg-cover bg-fixed">
-            <div className="bg-black w-full h-full lg:bg-opacity-50">
-                <nav className="px-12 py-5">
-                    <img src="/images/logo.png" alt="Logo" className="h-12" />
-                </nav>
-                <div className="flex justify-center">
-                    <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:max-w-md routed-md w-full">
-                        <h2 className="text-white text-4xl mb-8 font-semibold">
-                            Entrar
-                        </h2>
-                        <div className="flex flex-col gap-4"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-}
+import Input from "@/components/input";
+import { useCallback, useState } from "react";
 
-export default Auth 
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [variant, setVariant] = useState("login");
+
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) =>
+      currentVariant == "login" ? "register" : "login"
+    );
+  }, []);
+
+  return (
+    <div className="relative h-full w-full bg-[url('/images/background.jpg')] bg-no-repeat bg-center bg-cover bg-fixed">
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
+        <nav className="px-12 py-5">
+          <img src="/images/logo.png" alt="Logo" className="h-12" />
+        </nav>
+        <div className="flex justify-center">
+          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:max-w-md routed-md w-full">
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              {variant == "login" ? "Entrar" : "Registre-se"}
+            </h2>
+            <div className="flex flex-col gap-4">
+              {variant == "register" && (
+                <Input
+                  label="Nome"
+                  onChange={(ev: any) => setName(ev.target.value)}
+                  id="name"
+                  value={name}
+                />
+              )}
+              <Input
+                label="Email"
+                onChange={(ev: any) => setEmail(ev.target.value)}
+                id="email"
+                type="email"
+                value={email}
+              />
+              <Input
+                label="Senha"
+                onChange={(ev: any) => setPassword(ev.target.value)}
+                id="password"
+                type="password"
+                value={password}
+              />
+            </div>
+            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+              {variant == "login" ? "Entrar" : "Registre-se"}
+            </button>
+            <p className="text-neutral-500 mt-12">
+              {variant == "login" ? "Primeira vez no Netflix?" : "Já tenho conta no Netflix."}
+              <span
+                onClick={toggleVariant}
+                className="text-white ml-1 hover:underline cursor-pointer"
+              >
+               {variant == "login" ? "Crie uma conta." : "Entrar."}
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
